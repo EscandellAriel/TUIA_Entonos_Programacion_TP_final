1c556a870f9936f317ce0c29295f3b712d0acc68
Ariel <ariel.escandell@gmail.com>
2022-07-31 20:03:29 -0300

    Mejora usabilidad menu

--- menuPrincipal.sh
+++ menuPrincipal.sh
@@ -6,5 +6,7 @@ do
 [ -e $REPLY ] && echo "Elegir opcion valida!" && continue
 [ $REPLY == 6 ] && echo "Cerrando Menú" && break
 echo "Script elegido: " $OPCION && . $OPCION $1
+echo "1) statsWords.sh         3) findNames.sh          5) blankLinesCounter.sh
+2) statsUsageWords.sh    4) statsSentences.sh     6) Exit"
 done
 exit 0
--- statsSentences.sh
+++ statsSentences.sh
@@ -1,3 +1,5 @@
+#!bin/bash
+
 awk  'BEGIN{RS="\\. "}1' $1| grep -Ev "^$"  |  awk 'length > max_length { max_length = length; longest = $0 } END { print "Oración mas larga:"longest "\nCaracteres: "max_length }'
 awk  'BEGIN{RS="\\. "}1' $1| grep -Ev "^$"  |  awk 'NR==1 || length < min_length { min_length = length; shortest = $0 } END { print "Oración mas corta:"shortest"\nCaracteres: " min_length }'
 awk  'BEGIN{RS="\\. "}1' $1| grep -Ev "^$"  |  awk '{ suma += length; contador += 1}  END { print "Longitud promedio de oraciñon: "suma/contador}'
